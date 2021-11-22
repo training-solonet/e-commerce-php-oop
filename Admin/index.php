0081797506c0d157d07fe7b5323849acc227495a
megakusuma24 <mega.xrpla.19@gmail.com>
2021-11-22 09:35:21 +0700

    admin

---
+++ Admin/index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<!-- Head -->
+	<head>
+		<title>Dashboard | WARISANIFY</title>
+
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+
+		
+		<!-- Web Fonts -->
+		<link href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+
+		<!-- Components Vendor Styles -->
+		<link rel="stylesheet" href="../Assets/awesome/assets/vendor/themify-icons/themify-icons.css">
+		<link rel="stylesheet" href="../Assets/awesome/assets/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css">
+
+		<!-- Theme Styles -->
+		<link rel="stylesheet" href="../Assets/awesome/assets/css/theme.css">
+	</head>
+	<!-- End Head -->
+
+	<!-- Body -->
+	<body>
+		<!-- Header (Topbar) -->
+		<header class="u-header">
+			<!-- Header Left Section -->
+			<div class="u-header-left">
+				<!-- Header Logo -->
+				<a class="u-header-logo" href="index.html">
+				<h2>WARISANIFY</h2>	
+					
+				</a>
+				<!-- End Header Logo -->
+			</div>
+			<!-- End Header Left Section -->
+
+			<!-- Header Middle Section -->
+			<div class="u-header-middle">
+				<!-- Sidebar Invoker -->
+				<div class="u-header-section">
+					<a class="js-sidebar-invoker u-header-invoker u-sidebar-invoker" href="#"
+					   data-is-close-all-except-this="true"
+					   data-target="#sidebar">
+						<span class="ti-align-left u-header-invoker__icon u-sidebar-invoker__icon--open"></span>
+						<span class="ti-align-justify u-header-invoker__icon u-sidebar-invoker__icon--close"></span>
+					</a>
+				</div>
+				<!-- End Sidebar Invoker -->
+
+				
+				
+			
+
+				
+
+
+			</div>
+			<!-- End Header Middle Section -->
+		</header>
+		<!-- End Header (Topbar) -->
+
+		<!-- Main -->
+		<main class="u-main">
+			<!-- Sidebar -->
+			<aside id="sidebar" class="u-sidebar">
+				<div class="u-sidebar-inner">
+					<!-- Sidebar Header -->
+					<header class="u-sidebar-header">
+						<!-- Sidebar Logo -->
+						<a class="u-sidebar-logo" href="index.html">
+							<h3>WARISANIFY</h3>
+						</a>
+						<!-- End Sidebar Logo -->
+					</header>
+					<!-- End Sidebar Header -->
+
+					<!-- Sidebar Nav -->
+					<nav class="u-sidebar-nav">
+						<!-- Sidebar Nav Menu -->
+						<ul class="u-sidebar-nav-menu u-sidebar-nav-menu--top-level">
+							<!-- Dashboard -->
+							<li class="u-sidebar-nav-menu__item">
+								<a class="u-sidebar-nav-menu__link active" href="index.html">
+									<span class="ti-dashboard u-sidebar-nav-menu__item-icon"></span>
+									<span class="u-sidebar-nav-menu__item-title">Dashboard</span>
+								</a>
+							</li>
+							<!-- End Dashboard -->
+
+							<!-- UI Base -->
+							<li class="u-sidebar-nav-menu__item">
+								<a class="u-sidebar-nav-menu__link" href="#"
+								   data-target="#menuItemUIBase">
+								   <span class="ti-layout u-sidebar-nav-menu__item-icon"></span>
+									<span class="u-sidebar-nav-menu__item-title">Data Table</span>
+									<span class="ti-angle-down u-sidebar-nav-menu__item-arrow"></span>
+								</a>
+
+								<ul id="menuItemUIBase" class="u-sidebar-nav-menu u-sidebar-nav-menu--second-level" style="display: none;">
+									<!-- Colors -->
+									<li class="u-sidebar-nav-menu__item">
+										<a class="u-sidebar-nav-menu__link" href="tables.html">
+											<span class="u-sidebar-nav-menu__item-title">Data Barang</span>
+										</a>
+									</li>
+									<!-- End Colors -->
+
+									<!-- Typography -->
+									<li class="u-sidebar-nav-menu__item">
+										<a class="u-sidebar-nav-menu__link" href="ui-typography.html">
+											<span class="u-sidebar-nav-menu__item-title">Data Kategori</span>
+										</a>
+									</li>
+									<!-- End Typography -->
+								</ul>
+							</li>
+							<!-- End UI Base -->
+						
+						</ul>
+						<!-- End Sidebar Nav Menu -->
+					</nav>
+					<!-- End Sidebar Nav -->
+				</div>
+			</aside>
+			<!-- End Sidebar -->
+
+			<!-- Content -->
+			<div class="u-content">
+				<!-- Content Body -->
+				<div class="u-body">
+					
+					<div class="row">
+						
+						
+					</div>
+				</div>
+				<!-- End Content Body -->
+
+				<!-- Footer -->
+				<footer class="u-footer d-md-flex align-items-md-center text-center text-md-left text-muted">
+					<!-- Footer Menu -->
+					<ul class="list-inline mb-3 mb-md-0">
+						<li class="list-inline-item mr-4">
+							<a class="text-muted" href="https://www.facebook.com/htmlstream" target="_blank">About Htmlstream</a>
+						</li>
+						<li class="list-inline-item">
+							<a class="text-muted" href="https://htmlstream.com/" target="_blank">More Freebies</a>
+						</li>
+					</ul>
+					<!-- End Footer Menu -->
+
+					<!-- Copyright -->
+					<span class="text-muted ml-auto">&copy; 2019 <a class="text-muted" href="https://htmlstream.com/" target="_blank">Htmlstream</a>. All Rights Reserved.</span>
+					<!-- End Copyright -->
+				</footer>
+				<!-- End Footer -->
+			</div>
+			<!-- End Content -->
+		</main>
+		<!-- End Main -->
+
+		<!-- Global Vendor -->
+		<script src="../Assets/awesome/assets/vendor/jquery/dist/jquery.min.js"></script>
+		<script src="../Assets/awesome/assets/vendor/jquery-migrate/jquery-migrate.min.js"></script>
+		<script src="../Assets/awesome/assets/vendor/popper.js/dist/umd/popper.min.js"></script>
+		<script src="../Assets/awesome/assets/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+
+		<!-- Plugins -->
+		<script src="../Assets/awesome/assets/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js"></script>
+		<script src="../Assets/awesome/assets/vendor/chart.js/dist/Chart.min.js"></script>
+		<script src="../Assets/awesome/assets/vendor/chartjs-plugin-style/dist/chartjs-plugin-style.min.js"></script>
+
+		<!-- Initialization  -->
+		<script src="../Assets/awesome/assets/js/sidebar-nav.js"></script>
+		<script src="../Assets/awesome/assets/js/main.js"></script>
+
+		<script src="../Assets/awesome/assets/js/charts/area-chart.js"></script>
+		<script src="../Assets/awesome/assets/js/charts/area-chart-small.js"></script>
+		<script src="../Assets/awesome/assets/js/charts/doughnut-chart.js"></script>
+	</body>
+	<!-- End Body -->
+</html>
